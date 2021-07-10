44c3660adb1e5436b39d09e3aad25d0209edc6d8
unknown <aeneas62@hotmail.fr>
2021-07-11 00:29:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Service de recherche avec Elastic
---
+++ src/main/java/app/m2i/medic/MedicamentRechercheApplication.java
@@ -0,0 +1,13 @@
+package app.m2i.medic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicamentRechercheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicamentRechercheApplication.class, args);
+	}
+
+}
